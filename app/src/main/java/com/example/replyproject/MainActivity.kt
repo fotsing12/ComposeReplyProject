cf3412f97629b891e92aa9ff11fe3e9eb55fd4d8
fotsing12 <fotsingweber4@gmail.com>
2025-08-13 13:32:36 +0200

    first commit

---
+++ app/src/main/java/com/example/replyproject/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.replyproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.replyproject.ui.theme.ReplyProjectTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ReplyProjectTheme {
+                val localContext = LocalContext.current
+                val metrics =
+            }
+        }
+    }
+}
+
+@Composable
+fun PhoneView(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ReplyProjectTheme {
+        Greeting("Android")
+    }
+}
